4d15a794cc79b05e084d668612176d82a6244471
katkes <keshank1@hotmail.com>
2024-11-20 23:56:54 -0500

    Moved fetching and creating api info to separate methods

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,5 +1,10 @@
 from datetime import datetime
-from Creds import connection
+from Creds import connection, stmHeaders
+from google.protobuf.json_format import MessageToJson
+import csv
+import requests
+import json
+import gtfs_pb2
 
 day_of_week_mapping = {
     "Lundi": "Monday",
@@ -24,30 +29,6 @@ stm_metro_line = {
     "Ligne jaune":4, 
     "Ligne bleue":5
 }
-
-def normalize_time(time_str):
-    try:
-        # Split the time string into hours and minutes
-        hours, minutes = map(int, time_str.split(':'))
-        # Normalize the hours if they are 24 or more
-        if hours >= 24:
-            hours -= 24
-        # Create a datetime object with the normalized time
-        time_obj = datetime.strptime(f"{hours:02}:{minutes:02}", "%H:%M")
-        return time_obj.strftime("%H:%M")
-    except ValueError:
-        # Handle invalid time format
-        return None
-
-def extract_line_name(line_string):
-    parts = line_string.split(" - ")
-    if len(parts) > 1:
-        return parts[1]
-    else:
-        return parts[0]
-    
-def epoch_to_date(epoch):
-    return datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
 
 def table_creation():
     cursor = connection.cursor()
@@ -133,3 +114,124 @@ def table_creation():
     cursor.execute(create_table_query)
     connection.commit()
 
+def normalize_time(time_str):
+    try:
+        # Split the time string into hours and minutes
+        hours, minutes = map(int, time_str.split(':'))
+        # Normalize the hours if they are 24 or more
+        if hours >= 24:
+            hours -= 24
+        # Create a datetime object with the normalized time
+        time_obj = datetime.strptime(f"{hours:02}:{minutes:02}", "%H:%M")
+        return time_obj.strftime("%H:%M")
+    except ValueError:
+        # Handle invalid time format
+        return None
+
+def extract_line_name(line_string):
+    parts = line_string.split(" - ")
+    if len(parts) > 1:
+        return parts[1]
+    else:
+        return parts[0]
+    
+def epoch_to_date(epoch):
+    return datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
+
+def insert_into_bus_metro_stop_line_tables(cursor):
+    # Insert data into the stm_metro_line and stm_bus_line tables
+    with open('ConstantInformation/gtfs_stm/routes.txt', mode='r', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            line_name = extract_line_name(row['route_long_name'])
+            if "Ligne" in row['route_long_name']:
+                color_name = french_english_color_mapping.get(line_name, line_name)
+                insert_query = "INSERT INTO stm_metro_line (stm_metro_line_id, line_colour, line_number) VALUES (%s, %s, %s);"
+                cursor.execute(insert_query, (row['route_id'], color_name, row['route_id']))
+            else:
+                insert_query = "INSERT INTO stm_bus_line (stm_bus_line_id, line_name, line_number) VALUES (%s, %s, %s);"
+                cursor.execute(insert_query, (row['route_id'], line_name, row['route_id']))
+    
+    # Insert data into the stm_metro_stop and stm_bus_stop tables
+    with open('ConstantInformation/gtfs_stm/stops.txt', mode='r', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if "STATION" in row['stop_name']:
+                insert_query = "INSERT INTO stm_metro_stop (stm_metro_stop_id, stm_metro_stop_name, stm_metro_stop_code) VALUES (%s, %s, %s);"
+                cursor.execute(insert_query, (row['stop_id'], row['stop_name'], row['stop_code']))
+            else: 
+                insert_query = "INSERT INTO stm_bus_stop (stm_bus_stop_id, stm_bus_stop_name, stm_bus_stop_code) VALUES (%s, %s, %s);"
+                cursor.execute(insert_query, (row['stop_id'], row['stop_name'], row['stop_code']))
+    
+    connection.commit()
+
+def fetch_and_create_json_stm_response_json(url):
+    stm_response = requests.get(url, headers=stmHeaders)
+    if "gtfs-rt" in url:
+       if stm_response.status_code == 200:
+            try:
+               # Parse the Protobuf data from the response content
+                trip_updates = gtfs_pb2.FeedMessage()
+                trip_updates.ParseFromString(stm_response.content)
+                
+                # Convert the Protobuf message to JSON
+                json_trip_updates = MessageToJson(trip_updates)
+                
+                # Write the JSON data to stm_response_trips.json
+                with open('stm_response_trips.json', 'w') as json_file:
+                    json.dump(json.loads(json_trip_updates), json_file, indent=4)
+                
+                print("Protobuf response has been written to stm_response_trips.json")
+            except Exception as e:
+                print(f"Error: {e}")
+    else:
+        if stm_response.status_code == 200:
+            stm_response_json = stm_response.json()
+            with open('stm_response_v1.json', 'w') as json_file:
+                json.dump(stm_response, json_file, indent=4)
+                print("Json response has been written to stm_response_v1.json")
+            if "v1" in url:
+                file_name = "stm_response_v1.json"
+            elif "v2" in url:
+                file_name = "stm_response_v2.json"
+            else:
+                print("Error: Invalid URL")
+                return
+            with open(file_name, 'w') as json_file:
+                json.dump(stm_response_json, json_file, indent=4)
+                print(f"Json response has been written to {file_name}")
+            
+        else:
+            print(f"Error: Received status code {stm_response.status_code} for URL {url}")
+
+def fetch_and_create_ville_de_montreal_json(url):
+    number_of_records_per_request = 100
+    offset = 0
+    ville_de_montreal_base_url = "https://donnees.montreal.ca/api/3/action/datastore_search"
+    ville_de_montreal_data = []
+
+    if "518d9c92-89a3-408a-8ac4-04ee43e2ac9e" in url:
+        limit = 17200
+        file_name = "incidents_reseau_du_metro_all.json"
+    elif "534cdfd9-41e5-4e11-8675-738485509cce" in url: 
+        limit = 5400
+        file_name = "kilometrage_metro_planifie_all.json"
+    elif "c35e14b7-31b7-410d-9773-158bc30749df" in url:
+        limit = 31000
+        file_name = "kilometrage_metro_realise_all.json"
+    else:
+        print("Error: Invalid URL")
+        return
+    
+    while offset != limit:
+        ville_de_montreal_response = requests.get(f"{ville_de_montreal_base_url}?resource_id={url}&limit={number_of_records_per_request}&offset={offset}")
+        if ville_de_montreal_response.status_code == 200:
+            data = ville_de_montreal_response.json()
+            records = data.get("result", {}).get("records", [])
+            ville_de_montreal_data.extend(records)
+            offset += number_of_records_per_request
+
+    if ville_de_montreal_data:
+        with open(file_name, 'w') as json_file:
+            json.dump(ville_de_montreal_data, json_file, indent=4)
+            print(f"All data has been written to {file_name}")
--- StmApiFetch.py
+++ StmApiFetch.py
@@ -1,58 +1,21 @@
 import requests
-import json
-from google.protobuf.json_format import MessageToJson
-import gtfs_pb2 
-import csv
-from HelperFunctions import extract_line_name, epoch_to_date, table_creation, french_english_color_mapping
+from HelperFunctions import  epoch_to_date, table_creation, insert_into_bus_metro_stop_line_tables, fetch_and_create_json_stm_response_json, french_english_color_mapping
 from Creds import connection, stmHeaders
 
 cursor = connection.cursor()
+
 table_creation()
+insert_into_bus_metro_stop_line_tables(cursor)
 
-# Insert data into stm_metro_line and stm_bus_line tables
-with open('ConstantInformation/gtfs_stm/routes.txt', mode='r', encoding='utf-8-sig') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        line_name = extract_line_name(row['route_long_name'])
-        if "Ligne" in row['route_long_name']:
-            color_name = french_english_color_mapping.get(line_name, line_name)
-            insert_query = "INSERT INTO stm_metro_line (stm_metro_line_id, line_colour, line_number) VALUES (%s, %s, %s);"
-            cursor.execute(insert_query, (row['route_id'], color_name, row['route_id']))
-        else:
-            insert_query = "INSERT INTO stm_bus_line (stm_bus_line_id, line_name, line_number) VALUES (%s, %s, %s);"
-            cursor.execute(insert_query, (row['route_id'], line_name, row['route_id']))
+# Fetch and save response for version 1 and 2 of the service status API, as well as live location data into corresponding JSON files
+fetch_and_create_json_stm_response_json("https://api.stm.info/pub/od/i3/v1/messages/etatservice")
+fetch_and_create_json_stm_response_json("https://api.stm.info/pub/od/i3/v2/messages/etatservice")
+fetch_and_create_json_stm_response_json("https://api.stm.info/pub/od/gtfs-rt/ic/v2/tripUpdates")
 
-#Insert data into stm_metro_stop and stm_bus_stop tables
-with open('ConstantInformation/gtfs_stm/stops.txt', mode='r', encoding='utf-8-sig') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        if "STATION" in row['stop_name']:
-            insert_query = "INSERT INTO stm_metro_stop (stm_metro_stop_id, stm_metro_stop_name, stm_metro_stop_code) VALUES (%s, %s, %s);"
-            cursor.execute(insert_query, (row['stop_id'], row['stop_name'], row['stop_code']))
-        else: 
-            insert_query = "INSERT INTO stm_bus_stop (stm_bus_stop_id, stm_bus_stop_name, stm_bus_stop_code) VALUES (%s, %s, %s);"
-            cursor.execute(insert_query, (row['stop_id'], row['stop_name'], row['stop_code']))
-
-connection.commit()
-
-
-# # Fetch and save response for version 1
-# serviceStatusApiUrlV1 = "https://api.stm.info/pub/od/i3/v1/messages/etatservice"
-# stmServiceStatusResponseV1 = requests.get(serviceStatusApiUrlV1, headers=stmHeaders)
-# if stmServiceStatusResponseV1.status_code == 200:
-#     stmResponseV1 = stmServiceStatusResponseV1.json()
-#     with open('stm_response_v1.json', 'w') as json_file:
-#         json.dump(stmResponseV1, json_file, indent=4)
-#         print("Json response has been written to stm_response_v1.json")
-# else:
-#     print(f"Error: Received status code {stmServiceStatusResponseV1.status_code} for URL {serviceStatusApiUrlV1}")
-
-# Fetch and save response for version 2
 serviceStatusApiUrlV2 = "https://api.stm.info/pub/od/i3/v2/messages/etatservice"
 stmServiceStatusResponseV2 = requests.get(serviceStatusApiUrlV2, headers=stmHeaders)
 
 stm_bus_stop_cancelled_moved_relocated_id = 1
-
 for record in stmServiceStatusResponseV2.json().get("alerts"):
 
     description_texts = record.get("description_texts", [])
@@ -78,56 +41,3 @@ for record in stmServiceStatusResponseV2.json().get("alerts"):
                 )
                 stm_bus_stop_cancelled_moved_relocated_id += 1
             connection.commit()
-
-        # print(record.get("active_periods").get("start"))
-        # print(record.get("active_periods"))
-        # time_of_cancellation = epoch_to_date(record.get("active_periods").get("start"))
-        # print(time_of_cancellation)
-
-
-        # cursor.execute("SELECT stm_bus_stop_id FROM stm_bus_stop WHERE stm_bus_stop_code = %s", (stop_code,))
-        # result = cursor.fetchone()
-        # if result:
-        #     stm_bus_stop_id = result[0]
-
-        # cursor.execute(
-        #             "INSERT INTO stm_bus_stop_cancelled_moved_relocated (stm_bus_stop_cancelled_moved_relocated_id, stm_bus_stop_id, stm_bus_stop_code, stm_bus_stop_cancelled_moved_relocated_date, stm_bus_stop_cancelled_moved_relocated_reason) VALUES (%s, %s, %s, %s, %s)",
-        #             (stm_bus_stop_cancelled_moved_relocated_id, stm_bus_stop_id, stop_code, time_of_cancellation, reason)
-        #         )
-        # stm_bus_stop_cancelled_moved_relocated_id += 1
-
-
-# if stmServiceStatusResponseV2.status_code == 200:
-#     stmResponseV2 = stmServiceStatusResponseV2.json()
-#     with open('stm_response_v2.json', 'w') as json_file:
-#         json.dump(stmResponseV2, json_file, indent=4)
-#         print("Json response has been written to stm_response_v2.json")
-# else:
-#     print(f"Error: Received status code {stmServiceStatusResponseV2.status_code} for URL {serviceStatusApiUrlV2}")
-
-# Fetch and save trip updates
-stmTripUpdatesApiUrl = "https://api.stm.info/pub/od/gtfs-rt/ic/v2/tripUpdates"
-stmTripUpdatesResponse = requests.get(stmTripUpdatesApiUrl, headers=stmHeaders)
-if stmTripUpdatesResponse.status_code == 200:
-    try:
-        # Parse the Protobuf data from the response content
-        trip_updates = gtfs_pb2.FeedMessage()
-        trip_updates.ParseFromString(stmTripUpdatesResponse.content)
-        
-        # Convert the Protobuf message to JSON
-        json_trip_updates = MessageToJson(trip_updates)
-        
-        # Write the JSON data to stm_response_trips.json
-        with open('stm_response_trips.json', 'w') as json_file:
-            json.dump(json.loads(json_trip_updates), json_file, indent=4)
-        
-        print("Protobuf response has been written to stm_response_trips.json")
-    except Exception as e:
-        print(f"Error: {e}")
-else:
-    print(f"Error: Received status code {stmTripUpdatesResponse.status_code} for URL {stmTripUpdatesApiUrl}")
-    print(stmTripUpdatesResponse.text)
-
-
-cursor.close()
-connection.close()
