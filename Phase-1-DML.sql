71f4e8e1a4f4b2c8fb2fe60cc3e8ac4a9f95b287
Tahsin Islam <tahsin_i12@outlook.com>
2024-11-22 17:58:27 -0500

    added more DML queries for Tahsin

--- Phase-1-DML.sql
+++ Phase-1-DML.sql
@@ -71,3 +71,81 @@ FULL JOIN
     stm_bus_stop_cancelled_moved_relocated c
 ON 
     b.stm_bus_stop_code = c.stm_bus_stop_code;
+
+--correlated queries
+--above average kilometrage metro lines
+SELECT 
+    m.stm_metro_line_id,
+    m.planned_kilometerage,
+    m.day_of_week
+FROM 
+    stm_metro_planned_kilometerage m
+WHERE 
+    m.planned_kilometerage > (
+        SELECT AVG(m2.planned_kilometerage)
+        FROM stm_metro_planned_kilometerage m2
+        WHERE m2.day_of_week = m.day_of_week
+    );
+
+--bus stops with more than one planned kilometerage entry
+SELECT 
+    p.stm_metro_line_id
+FROM 
+    stm_metro_planned_kilometerage p
+WHERE 
+    (SELECT COUNT(*)
+     FROM stm_metro_planned_kilometerage p2
+     WHERE p2.stm_metro_line_id = p.stm_metro_line_id) > 1;
+
+--metro lines with planned kilometerage above the line average
+SELECT 
+    p.stm_metro_line_id,
+    p.planned_kilometerage
+FROM 
+    stm_metro_planned_kilometerage p
+WHERE 
+    p.planned_kilometerage > (
+        SELECT AVG(p2.planned_kilometerage)
+        FROM stm_metro_planned_kilometerage p2
+        WHERE p2.stm_metro_line_id = p.stm_metro_line_id
+    );
+
+--specific metro line incicents (green cuz its actually bad)
+CREATE OR REPLACE VIEW green_line_incidents AS
+SELECT 
+    i.stm_incident_id,
+    i.stm_incident_type,
+    i.stm_incident_date_of_incident,
+    i.stm_incident_location_of_incident
+FROM 
+    stm_incident i
+JOIN 
+    stm_metro_line m
+ON 
+    i.stm_metro_line_id = m.stm_metro_line_id
+WHERE 
+    m.line_colour = 'Green';
+
+--bus stops with no cancellation or relocation (USING NOT IN)
+SELECT stm_bus_stop_id
+FROM stm_bus_stop
+WHERE stm_bus_stop_id NOT IN (
+    SELECT stm_bus_stop_id
+    FROM stm_bus_stop_cancelled_moved_relocated
+);
+
+--using NOT EXISTS
+SELECT stm_bus_stop_id
+FROM stm_bus_stop bs
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM stm_bus_stop_cancelled_moved_relocated bsc
+    WHERE bs.stm_bus_stop_id = bsc.stm_bus_stop_id
+);
+
+--using EXCEPT
+SELECT stm_bus_stop_id
+FROM stm_bus_stop
+EXCEPT
+SELECT stm_bus_stop_id
+FROM stm_bus_stop_cancelled_moved_relocated;
