d0acc6d90f391afe0b89ee6b6878ad62c3b7b40b
Midhurshaan <midhur-2002@hotmail.com>
2024-11-25 00:03:49 -0500

    Updated DML scripts to reflect the changes made to database schema as well as added comments above each query to easily identify queries

--- Phase-1-DML.sql
+++ Phase-1-DML.sql
@@ -5,7 +5,6 @@ FROM stm_bus_stop_cancelled_moved_relocated
 WHERE stm_bus_stop_code = 54351;
 
 -- Task: Basic select with simple group by clause (with and without having clause). 
--- BY TAHSIN
 -- Done: Total planned kilometerage for each metro line grouped by day of week
 SELECT 
     stm_metro_route_id,
@@ -21,17 +20,16 @@ ORDER BY
 -- Task: A simple join query as well as its equivalent implementation using cartesian product and where clause.
 -- Done: Using simple JOIN
 SELECT *
-FROM stm_metro_realized_kilometrage rk JOIN stm_metro_line sl on rk.stm_metro_line_id = sl.stm_metro_line_id;
+FROM stm_metro_realized_kilometerage rk JOIN stm_metro_route smr on rk.stm_metro_route_id = smr.stm_metro_route_id;
 
 -- Task: A simple join query as well as its equivalent implementation using cartesian product and where clause.
 -- Done: Using cartesian product and WHERE clause
 SELECT *
-FROM stm_metro_realized_kilometrage rk
-CROSS JOIN stm_metro_line sl;
-WHERE rk.stm_metro_line_id = sl.stm_metro_line_id;
+FROM stm_metro_realized_kilometerage rk
+CROSS JOIN stm_metro_route smr
+WHERE rk.stm_metro_route_id = smr.stm_metro_route_id;
 
 -- Task: A few queries to demonstrate various join types on the same tables: inner vs. outer (left and right) vs. full join. Use of null values in the database to show the differences is required.
--- BY TAHSIN
 -- Done: Inner join
 SELECT 
     b.stm_bus_stop_id, 
@@ -45,7 +43,6 @@ ON
     b.stm_bus_stop_id = c.stm_bus_stop_id;
 
 -- Task: A few queries to demonstrate various join types on the same tables: inner vs. outer (left and right) vs. full join. Use of null values in the database to show the differences is required.
--- BY TAHSIN
 -- Done: Outer left join
 SELECT 
     b.stm_bus_stop_id, 
@@ -72,7 +69,6 @@ ON
 b.stm_bus_stop_id = c.stm_bus_stop_id;
     
 -- Task: A few queries to demonstrate various join types on the same tables: inner vs. outer (left and right) vs. full join. Use of null values in the database to show the differences is required.
--- BY TAHSIN
 -- Done: Full outer 
 SELECT 
     b.stm_bus_stop_id, 
@@ -86,7 +82,6 @@ ON
     b.stm_bus_stop_id = c.stm_bus_stop_id;
 
 -- Task: A couple of examples to demonstrate correlated queries.
--- BY TAHSIN
 -- Done: Display above average kilometrage metro lines using correlated queries
 SELECT 
     m.stm_metro_route_id,
@@ -102,7 +97,6 @@ WHERE
     );
 
 -- Task: An example of a view that has a hard-coded criteria, by which the content of the view may change upon changing the hard-coded value.
--- BY TAHSIN
 -- Done: View the incidents on a specific line (Green line) 
 CREATE OR REPLACE VIEW green_line_incidents AS
 SELECT 
@@ -121,7 +115,6 @@ WHERE
 
 
 --bus stops with more than one planned kilometerage entry
--- BY TAHSIN
 SELECT 
     p.stm_metro_route_id
 FROM 
@@ -134,7 +127,6 @@ GROUP BY
     p.stm_metro_route_id;
 
 --metro lines with planned kilometerage above the line average
--- BY TAHSIN
 SELECT 
     p.stm_metro_route_id,  
     p.planned_kilometerage
@@ -150,7 +142,6 @@ WHERE
 
 -- Task: Two implementations of the division operator using a) a regular nested query using NOT IN and b) a correlated nested query using NOT EXISTS and EXCEPT
 -- Done: A regular nested query using NOT IN
--- BY TAHSIN
 -- Selects distinct metro routes that have records for every day of the week (Monday to Sunday) in the realized kilometerage data.
 SELECT DISTINCT r.stm_metro_route_id
 FROM stm_metro_realized_kilometrage r
@@ -166,7 +157,6 @@ WHERE NOT EXISTS (
 );
 
 -- Done: A correlated nested query using NOT EXISTS and EXCEPT
--- BY TAHSIN
 -- Selects metro routes that have incidents and planned kilometerage data for every day of the week (Monday to Sunday).
 SELECT m.stm_metro_route_id
 FROM stm_metro_route m
@@ -184,30 +174,34 @@ WHERE NOT EXISTS (
 );
 
 
--- demonstrating use of null values for undefined / non-applicable
+-- Task: A few queries to demonstrate use of Null values for undefined / non-applicable.
+-- Done: A query performing a left join between bus stops and cancelled bus stops (since not all bus stops have been cancelled/relocated)
 SELECT * 
 FROM stm_bus_stop sbs LEFT JOIN stm_bus_stop_cancelled_moved_relocated sbsc ON sbs.stm_bus_stop_id = sbsc.stm_bus_stop_id;
 
--- Using INTERSECT
-SELECT stm_metro_line_id
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Using INTERSECT to get metro route id where the primary cause of incident was 'Clientèle' and the realized kilometerage is greater than 160
+SELECT stm_metro_route_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
 INTERSECT
-SELECT stm_metro_line_id
-FROM stm_metro_realized_kilometrage
+SELECT stm_metro_route_id
+FROM stm_metro_realized_kilometerage
 WHERE realized_kilometerage > 160;
 
--- Achieving INTERSECT without using set operations
-SELECT stm_metro_line_id
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Achieving INTERSECT to get metro route id where the primary cause of incident was 'Clientèle' and the realized kilometerage is greater than 160 without using Set Operations (Using nested SELECT and IN)
+SELECT stm_metro_route_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
-AND stm_metro_line_id IN
-(SELECT stm_metro_line_id
-FROM stm_metro_realized_kilometrage
+AND stm_metro_route_id IN
+(SELECT stm_metro_route_id
+FROM stm_metro_realized_kilometerage
 WHERE realized_kilometerage > 160)
-GROUP BY stm_metro_line_id;
+GROUP BY stm_metro_route_id;
 
--- Using UNION
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Using UNION to get metro route id where the primary cause of incident was 'Clientèle' and the realized kilometerage is greater than 160
 SELECT stm_metro_line_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
@@ -216,36 +210,39 @@ SELECT stm_metro_line_id
 FROM stm_metro_realized_kilometrage
 WHERE realized_kilometerage > 160;
 
--- Achieving UNION without using set operations
-SELECT stm_metro_line_id
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Achieving UNION to get metro route id where the primary cause of incident was 'Clientèle' and the realized kilometerage is greater than 160 without using Set Operations (Using nested SELECT and IN)
+SELECT stm_metro_route_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
-OR stm_metro_line_id IN
-(SELECT stm_metro_line_id
-FROM stm_metro_realized_kilometrage
+OR stm_metro_route_id IN
+(SELECT stm_metro_route_id
+FROM stm_metro_realized_kilometerage
 WHERE realized_kilometerage > 160)
-GROUP BY stm_metro_line_id
-ORDER BY stm_metro_line_id DESC;
+GROUP BY stm_metro_route_id
+ORDER BY stm_metro_route_id DESC;
 
--- Using Differences (EXCEPT)
-SELECT stm_metro_line_id
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Using difference operation (EXCEPT) to get stm metro route id where the primary incident cause was 'Clientèle' and the metro route color is not green
+SELECT stm_metro_route_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
 EXCEPT
-SELECT stm_metro_line_id
-FROM stm_metro_line
-WHERE line_colour = 'Green';
+SELECT stm_metro_route_id
+FROM stm_metro_route
+WHERE stm_metro_route_colour = 'Green';
 
--- Achieving EXCEPT without using set operations
-SELECT stm_metro_line_id
+-- Task: One example per set operations: intersect, union, and difference vs. their equivalences without using set operations.
+-- Done: Achieving difference operation (EXCEPT) to get stm metro route id where the primary incident cause was 'Clientèle' and the metro route color is not green wihtout using Set Operations (Using nested SELECT and NOT IN)
+SELECT stm_metro_route_id
 FROM stm_incident
 WHERE stm_incident_primary_cause = 'Clientèle'
-AND stm_metro_line_id NOT IN
-(SELECT stm_metro_line_id
-FROM stm_metro_line
-WHERE line_colour = 'Green')
-GROUP BY stm_metro_line_id
-ORDER BY stm_metro_line_id DESC;
+AND stm_metro_route_id NOT IN
+(SELECT stm_metro_route_id
+FROM stm_metro_route
+WHERE stm_metro_route_colour = 'Green')
+GROUP BY stm_metro_route_id
+ORDER BY stm_metro_route_id DESC;
 
 -- Task: Two queries that demonstrate the overlap and covering constraints.
 -- Done: Query to demonstrate overlap constraint
